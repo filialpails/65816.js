ea1c68406b9bedcb20eef5d026410ca591ece653
filialpails <bobert_1234567890@hotmail.com>
2012-11-27 12:12:44 -0500

    simplify disassembler and fix address printing so bytes are padded with 0's

--- 65816.js
+++ 65816.js
@@ -7,6 +7,10 @@ function range(a, b, step) {
 	while (a + step <= b) A[A.length] = a += step;
 	return A;
 }
+function pad(n, l) {
+  var length = l || 2;
+  return n.length >= length ? n : new Array(length - n.length + 1).join('0') + n;
+}
 function _65816(layout) {
 	var A = 0;
 	var B = 0;
@@ -31,7 +35,7 @@ _65816.prototype.run = function(code) {
 };
 
 function absolute(b1, b2) {
- return "$" + b2.toString(16) + b1.toString(16);
+ return "$" + pad(b2.toString(16)) + pad(b1.toString(16));
 }
 function absoluteIndexedX(b1, b2) {
  return absolute(b1, b2) + ",X";
@@ -46,15 +50,15 @@ function absoluteIndirect(b1, b2) {
  return "(" + absolute(b1, b2) + ")";
 }
 function immediate(b1, b2) {
- return "#" + b2.toString(16) + b1.toString(16);
+ return "#" + pad(b2.toString(16)) + pad(b1.toString(16));
 }
 function relative(b1) {
- var num = parseint(b1, 16);
+ var num = b1.toString(16);
  if (num >> 7 === 1) num = ~num + 1;
  return num;
 }
 function direct(b1) {
- return "$" + b1.toString(16);
+ return "$" + pad(b1.toString(16));
 }
 function directIndexedY(b1) {
  return direct(b1) + ",Y";
@@ -72,7 +76,7 @@ function absoluteIndirectLong(b1, b2) {
  return "[" + absolute(b1, b2) + "]";
 }
 function absoluteLong(b1, b2, b3) {
- return "$" + b3.toString(16) + b2.toString(16) + b1.toString(16);
+ return "$" + pad(b3.toString(16)) + pad(b2.toString(16)) + pad(b1.toString(16));
 }
 function absoluteLongIndexedX(b1, b2, b3) {
  return absoluteLong(b1, b2, b3) + ",X";
@@ -86,11 +90,11 @@ function directIndirect(b1) {
 function directIndirectLong(b1) {
  return "[" + direct(b1) + "]";
 }
-function directIndirectIndexedLongY(b1) {
+function directIndirectLongIndexedY(b1) {
  return directIndirectLong(b1) + ",Y";
 }
 function relativeLong(b1, b2) {
- var num = parseint(b2 + b1, 16);
+ var num = (b2 + b1).toString(16);
  if (num >> 15 === 1) num = ~num + 1;
  return num;
 }
@@ -100,92 +104,112 @@ function stackRelative(b1) {
 function stackRelativeIndirectIndexedY(b1) {
  return "(" + stackRelative(b1) + "),Y";
 }
-var m = true;
-var x = true;
+var m = false;
+var x = false;
 var hexToASMFuncs = {
-	0x00: function(b1) { return "BRK"; },
-	0x01: function(b1) { return "ORA " + directIndexedIndirectX(b1); },
-	0x02: function() { return "COP"; },
-	0x03: function(b1) { return "ORA " + stackRelative(b1); },
-	0X06: function(b1) { return "ASL" },
-	0x08: function() { return "PHP" },
-	0x09: function(b1, b2) { return "ORA " + immediate(b1, b2); },
-	0X0A: function() { return "ASL" },
-	0X0E: function(b1, b2) { return "ASL" },
-	0x10: function(b1) { return "BPL"; },
-	0X16: function(b1) { return "ASL"; },
-	0x18: function() { return "CLC"; },
-	0X1E: function(b1, b2) { return "ASL" },
-	0x20: function(b1, b2) { return "JSR " + absolute(b1, b2); },
-	0x21: function(b1) { return "AND" },
-	0x23: function(b1) { return "AND" },
-	0x24: function(b1) { return "BIT " + direct(b1); },
-	0x25: function(b1) { return "AND" },
-	0x26: function(b1) { return "ROL " + direct(b1); },
-	0x27: function(b1) { return "AND" },
-	0x29: function(b1) { return "AND" },
-	0x2D: function(b1, b2) { return "AND" },
-	0x2F: function(b1, b2, b3) { return "AND" },
-	0x30: function(b1) { return "BMI"; },
-	0x31: function(b1) { return "AND" },
-	0x32: function(b1) { return "AND" },
-	0x33: function(b1) { return "AND" },
-	0x35: function(b1) { return "AND" },
-	0x37: function(b1) { return "AND" },
-	0x39: function(b1, b2) { return "AND" },
-	0x3D: function(b1, b2) { return "AND" },
-	0x3F: function(b1, b2, b3) { return "AND" },
-	0x42: function() { return "WDM"; },
-	0x50: function(b1) { return "BVC"; },
-	0x5c: function(b1, b2, b3) { return "JMP " + absoluteLong(b1, b2, b3); },
-	0x61: function(b1) { return "ADC " + directIndexedIndirectX(b1); },
-	0x63: function(b1) { return "ADC " + stackRelative(b1); },
-	0x65: function(b1) { return "ADC " + direct(b1); },
-	0x67: function(b1) { return "ADC " + directIndirectLong(b1); },
-	0x68: function() { return "PLA"; },
-	0x69: function(b1, b2) { return "ADC " + immediate(b1, b2); },
-	0x6A: function() { return "ROR"; },
-	0x6D: function(b1, b2) { return "ADC " + absolute(b1, b2); },
-	0x6F: function(b1, b2, b3) { return "ADC " + absoluteLong(b1, b2, b3); },
-	0x70: function(b1, b2) { return "BVS"; },
-	0x71: function(b1) { return "ADC " + directIndirectIndexedY(b1, b2); },
-	0x72: function(b1) { return "ADC " + directIndirect(b1); },
-	0x73: function(b1) { return "ADC " + stackRelativeIndirectIndexedY(b1); },
-	0x75: function(b1) { return "ADC " + directIndexedX(b1); },
-	0x77: function(b1) { return "ADC " + directIndirectLongIndexedY(b1); },
-	0x79: function(b1, b2) { return "ADC " + absoluteIndexedY(b1, b2); },
-	0x7D: function(b1, b2) { return "ADC " + absoluteIndexedX(b1, b2); },
-	0x7F: function(b1, b2, b3) { return "ADC " + absoluteLongIndexedX(b1, b2, b3); },
-	0x80: function(b1) { return "BRA " + relative(b1); },
-	0x82: function(b1, b2) { return "BRL"; },
-	0x8D: function(b1, b2) { return "STA " + absolute(b1, b2); },
-	0x8F: function(b1, b2, b3) { return "STA " + absoluteLong(b1, b2, b3); },
-	0x90: function(b1) { return "BCC"; },
-	0xB0: function(b1) { return "BCS"; },
-	0xC2: function(b1) {
+	0x00: ["BRK", direct],
+	0x01: ["ORA", directIndexedIndirectX],
+	0x02: ["COP", null],
+	0x03: ["ORA", stackRelative],
+	0X06: ["ASL", direct],
+	0x08: ["PHP", null],
+	0x09: ["ORA", immediate],
+	0X0A: ["ASL", null],
+	0X0E: ["ASL", absolute],
+	0x10: ["BPL", relative],
+	0x12: ["ORA", directIndirect],
+	0x14: ["TRB", direct],
+	0x15: ["ORA", directIndexedX],
+	0X16: ["ASL", directIndexedX],
+	0x18: ["CLC", null],
+	0x1C: ["TRB", absolute],
+	0x1D: ["ORA", absoluteIndexedX],
+	0X1E: ["ASL", absoluteIndexedX],
+	0x1F: ["ORA", absoluteLongIndexedX],
+	0x20: ["JSR", absolute],
+	0x21: ["AND", directIndexedIndirectX],
+	0x23: ["AND", stackRelative],
+	0x24: ["BIT", direct],
+	0x25: ["AND", direct],
+	0x26: ["ROL", direct],
+	0x27: ["AND", directIndirectLong],
+	0x29: ["AND", immediate],
+	0x2C: ["BIT", absolute],
+	0x2D: ["AND", absolute],
+	0x2F: ["AND", absoluteLong],
+	0x30: ["BMI", relative],
+	0x31: ["AND", directIndirectIndexedY],
+	0x32: ["AND", directIndirect],
+	0x33: ["AND", stackRelativeIndirectIndexedY],
+	0x35: ["AND", directIndexedX],
+	0x37: ["AND", directIndirectLongIndexedY],
+	0x39: ["AND", absoluteIndexedY],
+	0x3D: ["AND", absoluteIndexedX],
+	0x3F: ["AND", absoluteLongIndexedX],
+	0x42: ["WDM", null],
+	0x50: ["BVC", relative],
+	0x54: ["MVN", blockMove],
+	0x5B: ["TCD", null],
+	0x5c: ["JMP", absoluteLong],
+	0x61: ["ADC", directIndexedIndirectX],
+	0x63: ["ADC", stackRelative],
+	0x65: ["ADC", direct],
+	0x67: ["ADC", directIndirectLong],
+	0x68: ["PLA", null],
+	0x69: ["ADC", immediate],
+	0x6A: ["ROR", null],
+	0x6D: ["ADC", absolute],
+	0x6F: ["ADC", absoluteLong],
+	0x70: ["BVS", relative],
+	0x71: ["ADC", directIndirectIndexedY],
+	0x72: ["ADC", directIndirect],
+	0x73: ["ADC", stackRelativeIndirectIndexedY],
+	0x75: ["ADC", directIndexedX],
+	0x77: ["ADC", directIndirectLongIndexedY],
+	0x79: ["ADC", absoluteIndexedY],
+	0x7B: ["TDC", null],
+	0x7D: ["ADC", absoluteIndexedX],
+	0x7F: ["ADC", absoluteLongIndexedX],
+	0x80: ["BRA", relative],
+	0x82: ["BRL", relativeLong],
+	0x85: ["STA", direct],
+	0x8D: ["STA", absolute],
+	0x8F: ["STA", absoluteLong],
+	0x90: ["BCC", relative],
+	0x9C: ["STZ", absolute],
+	0xA9: ["LDA", immediate],
+	0xAA: ["TAX", null],
+	0xB0: ["BCS", relative],
+	0xC2: ["REP", function(b1) {
 		if (b1 & 0x10 === 0x10) x = false;
 		if (b1 & 0x20 === 0x20) m = false;
-		return "REP " + immediate(b1, 0);
-	},
-	0xD0: function(b1) { return "BNE"; },
-	0xE2: function(b1) {
+		return "#%" + pad(b1.toString(2), 8);
+	}],
+	0xC9: ["CMP", immediate],
+	0xCE: ["DEC", absolute],
+	0xD0: ["BNE", relative],
+	0xE2: ["SEP", function(b1) {
 		if (b1 & 0x10 === 0x10) x = true;
 		if (b1 & 0x20 === 0x20) m = true;
-		return "SEP " + immediate(b1, 0);
-	},
-	0xEB: function() { return "XBA"; },
-	0xF0: function(b1) { return "BEQ"; },
-	0xFB: function() { return "XCE"; },
-	0xFF: function(b1, b2, b3) { return "SBC " + absoluteLongIndexedX(b1, b2, b3); },
+		return "#%" + pad(b1.toString(2), 8);
+	}],
+	0xEA: ["NOP", null],
+	0xEB: ["XBA", null],
+	0xF0: ["BEQ", relative],
+	0xFB: ["XCE", null],
+	0xFF: ["SBC", absoluteLongIndexedX]
 };
 _65816.disassemble = function(hex) {
 	var length = hex.length;
 	var asm = "";
 	for (var i = 0; i < length; ++i) {
-		var func = hexToASMFuncs[hex[i]];
-		var operandlength = func.length;
-		var args = new Array(operandlength);
 		var opcode = hex[i];
+		var func = hexToASMFuncs[opcode];
+		var mnemonic = func[0];
+		var mode = func[1];
+		if (!mode) mode = function() { return ""; };
+		var operandlength = mode.length;
+		var args = new Array(operandlength);
 		for (var j = 0; j < operandlength; ++j) {
 			args[j] = hex[++i];
 			if (m && [0x69, 0x29, 0x89, 0xc9, 0xe0, 0xc0, 0x49, 0xa9, 0x09, 0xe9].indexOf(hex[i]) != -1) {
@@ -197,8 +221,7 @@ _65816.disassemble = function(hex) {
 				break;
 			}
 		}
-		console.log("opcode: " + opcode.toString(16) + " " + args);
-		asm += func.apply(null, args) + "\n";
+		asm += mnemonic + " " + mode.apply(null, args) + "\n";
 	}
 	return asm;
 };
